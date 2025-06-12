3f37fc2bbd39295fa75f691067eca689c65e9b1d
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-06-12 14:48:05 +0530

    Implement dynamic grid creation with JavaScript and center grid using Flexbox
    - Added functions to generate a grid of rows and columns dynamically in the DOM.
    - Ensured each row contains the correct number of cells.
    - Applied CSS Flexbox to align grid rows horizontally and center the entire grid both vertically and horizontally within the pag

---
+++ jsfile.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('container')
+
+
+let cells = document.getElementsByClassName('cells')
+
+function grid(){
+    makeRows(20);
+    makeColumns(20);
+}
+
+function makeRows(rowsNums){
+    for(let i = 0; i < rowsNums; i++){
+        let row = document.createElement('div')
+        container.appendChild(row).className = "gridRow"
+    }
+}
+
+function makeColumns(cellNums){
+    let rows = document.getElementsByClassName('gridRow')
+    for(let i = 0; i < rows.length; i++){
+        for(let j = 0 ; j < cellNums ; j++){
+            let columns = document.createElement('div')
+            rows[i].appendChild(columns).className = 'cells'
+        }
+    }
+}
+
+grid()
